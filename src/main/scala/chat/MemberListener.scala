b59cbdcc0e3d2e9580e3d027468652b9a19f8ac6
Adam Mollenkopf <amollenkopf@esri.com>
2013-09-23 16:43:51 -0700

    Adjusted Unicode characters with =>
    Adjusted Unicode characters with => throughout template.

--- src/main/scala/chat/MemberListener.scala
+++ src/main/scala/chat/MemberListener.scala
@@ -20,18 +20,18 @@ class MemberListener extends Actor with ActorLogging {
   var nodes = Set.empty[Address]
 
   def receive = {
-    case state: CurrentClusterState ⇒
+    case state: CurrentClusterState =>
       nodes = state.members.collect {
-        case m if m.status == MemberStatus.Up ⇒ m.address
+        case m if m.status == MemberStatus.Up => m.address
       }
-    case MemberUp(member) ⇒
+    case MemberUp(member) =>
       nodes += member.address
       log.info("Member is Up: {}. {} nodes in cluster",
         member.address, nodes.size)
-    case MemberRemoved(member, _) ⇒
+    case MemberRemoved(member, _) =>
       nodes -= member.address
       log.info("Member is Removed: {}. {} nodes cluster",
         member.address, nodes.size)
-    case _: MemberEvent ⇒ // ignore
+    case _: MemberEvent => // ignore
   }
 }
